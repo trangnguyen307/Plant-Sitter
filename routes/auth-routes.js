ec476310fb850539f8b2653ed7096fe3fca35cdf
Claire Lacanal <lacanal.claire@gmail.com>
2020-11-17 21:00:04 +0100

    auth-routes on going

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,9 +1,91 @@
-const express = require('express');
-const router  = express.Router();
+const express    = require('express');
+const authRoutes = express.Router();
 
-/* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+const bcrypt     = require('bcryptjs');
+
+// require the user model !!!!
+const User       = require('../models/user-model');
+
+
+authRoutes.post('/signup', (req, res, next) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.passwordHash;
+  const avatar = req.body.avatar;
+  
+  if (!username || !email || !password) {
+    res.status(400).json({ message: 'Provide username, email and password' });
+    return;
+  }
+
+  if (password.length < 7) {
+    res.status(400).json({ message: 'Please make your password at least 8 characters long for security purposes.' });
+    return;
+  }
+  
+  User.findOne({ username, email })
+    .then(foundUser => {
+      if (foundUser) {
+        res.status(400).json({ message: 'Username taken. Choose another one.' });
+        return;
+      }
+    
+      const salt     = bcrypt.genSaltSync(10);
+      const hashPass = bcrypt.hashSync(password, salt);
+    
+      const aNewUser = new User({
+        username:username,
+        password: hashPass
+      });
+    
+      aNewUser.save()
+        .then(() => {
+          // Persist our new user into session
+          req.session.currentUser = aNewUser
+
+         res.status(200).json(aNewUser);
+        })
+        .catch(err => {
+          res.status(400).json({ message: 'Saving user to database went wrong.' });
+        })
+      ;
+    })
+    .catch(err => {
+      res.status(500).json({message: "Username check went bad."});
+    })
+  ;
 });
 
-module.exports = router;
+authRoutes.post('/login', (req, res, next) => {
+  const {username, password} = req.body
+
+  User.findOne({username}).then(user => {
+    if (!user) {
+      return next(new Error('No user with that username'))
+    }
+    
+    // compareSync
+    if (bcrypt.compareSync(password, user.password) !== true) {
+      return next(new Error('Wrong credentials'))
+    } else {
+      req.session.currentUser = user
+      res.json(user)
+    }
+  }).catch(next)
+});
+
+authRoutes.post('/logout', (req, res, next) => {
+  req.session.destroy()
+  res.json({message: 'Your are now logged out.'})
+});
+
+authRoutes.get('/loggedin', (req, res, next) => {
+  // req.isAuthenticated() is defined by passport
+  if (req.session.currentUser) {
+      res.status(200).json(req.session.currentUser);
+      return;
+  }
+  res.status(403).json({ message: 'Unauthorized' });
+});
+
+module.exports = authRoutes;

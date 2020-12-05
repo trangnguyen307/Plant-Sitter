f0f05307037cc118f0e348dc004ef7962e7c5d06
Claire Lacanal <lacanal.claire@gmail.com>
2020-12-05 10:44:43 +0100

    rien

--- client/src/components/profileUser/MyComments.js
+++ client/src/components/profileUser/MyComments.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
 import service from '../auth/auth-service';
-import HoverRating from '../Rating';
 import Rating from '@material-ui/lab/Rating';
 import './ProfileUser.css'
 

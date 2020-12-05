97c991b64f2d8ed1d112eb3f9b6fa543e214e7ba
trangnguyen <nttrang30792@gmail.com>
2020-12-05 10:38:37 +0100

    add date for annonce model

--- models/annonce-model.js
+++ models/annonce-model.js
@@ -4,13 +4,16 @@ const Schema   = mongoose.Schema;
 const annonceSchema = new Schema({
   type: String,
   moving: Boolean,
+  title: String,
   description: String,
   adress: String,
   author: { type : Schema.Types.ObjectId, ref: 'User' },
   imageUrl: {
     type: String,
     default : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM-VfnDe5n-tVS6ZpnKfMekZoX0o_V2_o7gw&usqp=CAU'
-  } 
+  },
+  startDate: String,
+  endDate: String 
 
 }, 
 {

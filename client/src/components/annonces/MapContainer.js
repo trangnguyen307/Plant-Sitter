6291f2fb6a8ac5ee5dadb825aed4dba43c716676
trangnguyen <nttrang30792@gmail.com>
2020-12-18 23:15:17 +0100

    add info window on map

--- client/src/components/annonces/MapContainer.js
+++ client/src/components/annonces/MapContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
+import './AnnoncesList.css'
 
 import Geocode from "react-geocode";
 Geocode.setApiKey(process.env.REACT_APP_GOOGLE_API_KEY);
@@ -17,12 +18,12 @@ class MapContainer extends React.Component {
     defaultCenter : {
       lat: 48.856613, lng: 2.352222
     },
-    locations: []
+    locations: [],
+    selected: {}
   }
   
   getLocations = (address) => {
     let locations=this.state.locations;
-    console.log('get location',locations,'address',address)
     address.map(item => Geocode.fromAddress(item.adress).then(
       response => {
         const { lat, lng } = response.results[0].geometry.location;
@@ -54,10 +55,32 @@ class MapContainer extends React.Component {
          zoom={13}
          center={this.state.defaultCenter}
        >
-         {/* {locations.length >0 && <Marker position={{lat:locations[0].location.lat,lng: locations[0].location.lng}} />} */}
          {
            
-           this.state.locations.map(item => <Marker key={item.name._id} position={{lat:item.location.lat, lng:item.location.lng}} name={item.name.title}/>)
+           this.state.locations.map(item => 
+            <Marker key={item.name._id} name={item.name.title}
+                position={{lat:item.location.lat, lng:item.location.lng}} 
+                onClick={()=>{
+                  this.setState({selected:item})
+                }}
+            />
+            )
+         }
+         {
+            this.state.selected.location && 
+            (
+              <InfoWindow
+              position={this.state.selected.location}
+              clickable={true}
+              onCloseClick={() => this.setState({selected:{}})}
+            >
+              <div className="info-window-map">
+                <img src={this.state.selected.name.imageUrl} alt='' />
+                <p className="title">{this.state.selected.name.title}</p>
+                <p>{this.state.selected.name.adress}</p>
+              </div>
+            </InfoWindow>
+            )
          }
        </GoogleMap>
     </LoadScript>
